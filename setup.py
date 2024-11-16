bbd6b7021af0361e9bc2d1ccf40b44b2685c3c9c
rk-geode <rajeshkumar.pivotal@gmail.com>
2024-11-17 01:15:47 +0530

    settinup the requirement to install the require library

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GST Chatbot',
+    version= '0.0.1',
+    author= 'Rajesh',
+    packages= find_packages(),
+    install_requires = []
+
+)
